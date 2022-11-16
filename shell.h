9815758c69d1bc8148ca453e55e728a446468ae1
Nightburnn <nightburn2003@gmail.com>
2022-11-16 05:04:51 -0800

    ...

--- shell.h
+++ shell.h
@@ -77,15 +77,10 @@ extern char **environ;
 
 
 /**
-
  * struct liststr - singly linked list
-
  * @num: the number field
-
  * @str: a string
-
  * @next: points to the next node
-
  */
 
 typedef struct liststr
@@ -103,47 +98,26 @@ typedef struct liststr
 
 
 /**
-
  *struct passinfo - contains pseudo-arguements to pass into a function,
-
  *		allowing uniform prototype for function pointer struct
-
  *@arg: a string generated from getline containing arguements
-
  *@argv: an array of strings generated from arg
-
  *@path: a string path for the current command
-
  *@argc: the argument count
-
  *@line_count: the error count
-
  *@err_num: the error code for exit()s
-
  *@linecount_flag: if on count this line of input
-
  *@fname: the program filename
-
  *@env: linked list local copy of environ
-
  *@environ: custom modified copy of environ from LL env
-
  *@history: the history node
-
  *@alias: the alias node
-
  *@env_changed: on if environ was changed
-
  *@status: the return status of the last exec'd command
-
  *@cmd_buf: address of pointer to cmd_buf, on if chaining
-
  *@cmd_buf_type: CMD_type ||, &&, ;
-
  *@readfd: the fd from which to read line input
-
  *@histcount: the history line number count
-
  */
 
 typedef struct passinfo
@@ -194,20 +168,16 @@ typedef struct passinfo
 
 #define INFO_INIT \
 
-{NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL, NULL, NULL, 0, 0, NULL, \
-
-	0, 0, 0}
+{
+	NULL, NULL, NULL, 0, 0, 0, 0, NULL, NULL, NULL,
+	NULL, NULL, 0, 0, NULL, \	0, 0, 0}
 
 
 
 /**
-
  *struct builtin - contains a builtin string and related function
-
  *@type: the builtin command flag
-
  *@func: the function
-
  */
 
 typedef struct builtin
